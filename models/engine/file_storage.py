a425d0c15d1da6673c00980862d317adc81bd6a6
BM-Daniel <danbabereyir@gmail.com>
2023-03-12 18:32:40 -0700

    Create and update files for models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,13 @@ This file contains the FileStorage class that converts between str and dict
 import json
 
 from models.base_model import BaseModel
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
